d4663f7cdf980e4eff37fa402ee4e0e5813f9781
Jooney <42194130+jhan756k@users.noreply.github.com>
2023-02-24 14:31:14 +0900

    Feb24

---
+++ Feb24.java
@@ -0,0 +1,22 @@
+public class Feb24 {
+    public static void main(String[] args) {
+        // 유클리드 호제법 GCD
+        int a = 480;
+        int b = 859912;
+
+        System.out.println(a+" "+b); 
+
+        while (a != 0 && b != 0) {
+            if (a > b) {
+                a %= b;
+            } else {
+                b %= a;
+            }
+            
+            System.out.println(a+" "+b); 
+        }
+        System.out.println(a+b);
+    }
+
+    // for (; a != 0 && b != 0;) --> 세미콜론으로 끝내면 됨
+}
