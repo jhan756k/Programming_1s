9ca6e14a9040ab246ab47fa7c825e16efaeb0d4d
Jooney <42194130+jhan756k@users.noreply.github.com>
2023-03-15 11:15:32 +0900

    Mar15

--- Mar13.java
+++ Mar13.java
@@ -1,12 +1,7 @@
 public class Mar13 {
     public static void main(String[] args) {
         /* 
-        Iteration vs Recursion
-        
-        스택에 의한 순서 차이 구분 (recurMe)
-        Stack --> Last In First Out (LIFO)
-        스택에는 순서대로 main, recurMe1, recurMe2가 쌓임
-        스택 쌓이는 순서 공부하기 (예시 recurMe)
+        Iteration vs Recursion 
         */
 
         int n = 5;
@@ -16,9 +11,6 @@ public class Mar13 {
         System.out.println(fib_recursion(n));
         System.out.println(gcd_recursion(12, 18));
         System.out.println(biCoefRecursion(5, 3));
-        recurMe1(n);
-        System.out.println();
-        recurMe2(n);
     }
 
     public static int addAll(int n){
@@ -45,21 +37,5 @@ public class Mar13 {
         // 나올지 안나올지 애매함 이항계수
         if (k==0 || k==n) return 1;
         else return biCoefRecursion(n-1, k-1) + biCoefRecursion(n-1, k);
-    }
-
-    public static void recurMe1(int n){
-        if (n==0) return;
-        else {
-            System.out.print(n + " ");
-            recurMe1(n-1);
-        }
-    }
-
-    public static void recurMe2(int n){
-        if (n==0) return;
-        else {
-            recurMe2(n-1);
-            System.out.print(n + " ");
-        }
     }
 }
---
+++ Mar15.java
@@ -0,0 +1,61 @@
+public class Mar15 {
+    public static void main(String[] args) {
+        /* 
+        스택에 의한 순서 차이 구분 (recurMe)
+        Stack --> Last In First Out (LIFO)
+        스택에는 순서대로 main, recurMe1, recurMe2가 쌓임
+        스택 쌓이는 순서 공부하기 (알파, 베타, 감마 체크포인트 적기)
+        */
+
+        int x = 30;
+
+        recurMe1(1);
+        System.out.println();
+        recurMe2(1);
+        System.out.println();
+        bin(x);
+        System.out.println();
+        bin_32(x, 1);
+    }
+
+    public static void recurMe1(int n){
+        System.out.print(n + " "); 
+        // 호출될 때마다 출력하므로 (n<4)까지 1 2 3 4
+        // (n>=4) 가 되면 스택이 점점 비워지는데 if 문 다음에 아무것도 없으므로 아무일도 안일어남)
+        if (n<4){
+            recurMe1(n+1);
+        }
+    }
+
+    public static void recurMe2(int n){
+        if (n<4){
+            recurMe2(n+1);
+        }
+        System.out.print(n + " "); 
+        // 모든 함수가 다 호출되고 나서 스택에 의해 출력되므로 4 3 2 1 
+        // F(1) -> F(2) -> F(3) -> F(4) -> F(3) -> F(2) -> F(1)
+        // 호ㅗㅗㅗㅗㅗㅗ출ㄹㄹㄹ -> __4_______3_______2_______1_ --> 출력 파트 
+    }
+
+    public static void bin(int n){
+        if (n==0) return;
+        else {
+            bin(n/2);
+            System.out.print(n%2); 
+            // 자동으로 순서 바뀌어서 출력됨
+        }
+    }
+
+    public static void bin_32(int a, int n){
+        if (n==33) return;
+        else {
+            bin_32(a/2, n+1);
+            if (n%4==0 && n!=32) System.out.print(" ");
+            System.out.print(a%2); 
+            // 자동으로 순서 바뀌어서 출력됨
+        }
+    }
+
+    // 모든 진법 에서 ABCDEF.. 포함한 범용 변환함수 printNotation 구현해보기
+    // MyRecursion.java 파일에 있음
+}
