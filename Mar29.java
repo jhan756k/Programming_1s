d93cd9a4eed9e6f7a3a8000de50c8dbf63851723
Jooney <42194130+jhan756k@users.noreply.github.com>
2023-03-29 18:49:54 +0900

    bubblesort

--- Mar29.java
+++ Mar29.java
@@ -9,6 +9,11 @@ public class Mar29 {
         1. 배열을 순회하여 최솟값을 찾아낸다
         2. 그값과 찾기 시작한 점과 교환한다. 
         - 한마디로 앞에서부터 최솟값을 하나씩 넣어준다.
+
+        -- 버블정렬 Bubble Sort --
+        1. 배열을 순회하면서 인접한 두개의 값을 비교하여 교환한다.
+        2. 한바퀴를 돌면 가장 큰값이 맨 뒤로 간다.
+        3. 1~2번을 배열의 길이-1 만큼 반복한다
         */
 
         // 문제 1
@@ -55,6 +60,10 @@ public class Mar29 {
             }
             if (x==5) System.out.println("두 배열은 같습니다.");
         }
+
+        int test[] = new int []{5, 2, 3, 1, 6, 4};
+        bubbleSort(test);
+        for (int i=0; i<test.length; i++) System.out.print(test[i] + " ");
     }
 
     public static int [] sortList(int [] arr) { // Selection Sort
@@ -70,4 +79,18 @@ public class Mar29 {
         // 아니면 minval, minindex 변수 잡아서 마지막에 한번만 대입해도 됨
         return arr;
     }
-}
+
+    public static int [] bubbleSort(int [] arr) {
+        for (int i=0; i<arr.length-1; i++){
+            for (int s=0; s<arr.length-1; s++){
+                if (arr[s] > arr[s+1]){
+                    int tmp = arr[s];
+                    arr[s] = arr[s+1];
+                    arr[s+1] = tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+} 
