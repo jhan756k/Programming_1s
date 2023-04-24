cb3bd01d8d687706ccc7ee0fccd58e5e9671c4e4
Jooney <42194130+jhan756k@users.noreply.github.com>
2023-04-24 16:40:13 +0900

    Apr21

---
+++ 기말고사/Apr21.java
@@ -0,0 +1,11 @@
+public class Apr21 {
+    public static void main(String[] args) {
+        /*
+        소프트웨어 위기 찾아보기
+        C+ --> JAVA
+        C+은 중간에 OOP도 아니고 그냥도 아닌 어중간한 명령형 언어였음.
+
+        
+        */
+    }
+}
