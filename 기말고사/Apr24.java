eadc7f60171b61c22ab86c5e52f390460b15ef80
Jooney <42194130+jhan756k@users.noreply.github.com>
2023-04-24 17:11:58 +0900

    정리

---
+++ 기말고사/Apr24.java
@@ -0,0 +1,10 @@
+public class Apr24 {
+    public static void main(String[] args) {
+        /*
+        Class PPT - Class 1 **매우중요**
+        [] --> 생략 가능
+
+        PPT - OOP의 8대 주요사항 -> For Reusability
+        */
+    }
+}
