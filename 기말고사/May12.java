c88a6cfb80110b08323b3124993828957280db91
Jooney <jhan756k@gmail.com>
2023-07-25 19:17:55 +0900

    final

--- 기말고사/May12.java
+++ 기말고사/May12.java
@@ -9,6 +9,21 @@ public class May12 {
         static 만으로는 오버로딩 안됨
         클래스 메소드는 인스턴스 변수에 접근할 수 없음
         */
+
+        Monkey as = new Monkey();
+        Dum bi = new Dum();
+        Monkey [] ss = new Monkey[10];
+        ss[0] = new Monkey();
+        ss[1] = new Dum();
+        ss[2] = as;
+        System.out.println(as.hashCode());
+
+        for (int i=0; i<ss.length; i++){
+            System.out.println(ss[i]);
+        }
+
+        System.out.println(bi instanceof Monkey);
+
     }
 
     public static class Monkey{
@@ -17,6 +32,12 @@ public class May12 {
         public Monkey(){
             counter++;
             System.out.println("I am the " + counter + "th monkey.");
+        }
+    }
+
+    public static class Dum extends Monkey{
+        public Dum(){
+            System.out.println("I am dumm");
         }
     }
 }
